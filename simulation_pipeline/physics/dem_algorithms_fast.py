759cd9629d6dab6271320a99f089bb99f17da8de
Will Barnes <will.t.barnes@gmail.com>
2022-06-10 17:00:05 -0400

    more jax fixes?

--- simulation_pipeline/physics/dem_algorithms_fast.py
+++ simulation_pipeline/physics/dem_algorithms_fast.py
@@ -116,19 +116,29 @@ def simple_reg_dem_numba(data, errors, exptimes, logt, tresps,
 
 def simple_reg_dem_jax(data, errors, exptimes, logt, tresps,
                        kmax=100, kcon=5, steps=[0.1, 0.5], drv_con=8.0, chi2_th=1.0, tol=0.1):
+    import jax
     from jax.scipy.linalg import cho_factor, cho_solve
+    import jax.numpy as jnp
+
+    # Move the input arrays to the device
+    data = jax.device_put(data)
+    errors = jax.device_put(errors)
+    exptimes = jax.device_put(exptimes)
+    logt = jax.device_put(logt)
+    tresps = jax.device_put(tresps)
+
     [nt, nd] = tresps.shape
-    nt_ones = np.ones(nt)
+    nt_ones = jnp.ones(nt)
     [nx, ny, nd] = data.shape
     dT = logt[1:nt]-logt[0:nt-1]
-    [dTleft, dTright] = [np.diag(np.hstack([dT, 0])), np.diag(np.hstack([0, dT]))]
-    [idTleft, idTright] = [np.diag(np.hstack([1.0/dT, 0])), np.diag(np.hstack([0, 1.0/dT]))]
-    Bij = ((dTleft+dTright)*2.0 + np.roll(dTright, -1, axis=0) + np.roll(dTleft, 1, axis=0))/6.0
+    [dTleft, dTright] = [jnp.diag(jnp.hstack([dT, 0])), jnp.diag(jnp.hstack([0, dT]))]
+    [idTleft, idTright] = [jnp.diag(jnp.hstack([1.0/dT, 0])), jnp.diag(jnp.hstack([0, 1.0/dT]))]
+    Bij = ((dTleft+dTright)*2.0 + jnp.roll(dTright, -1, axis=0) + jnp.roll(dTleft, 1, axis=0))/6.0
     # Matrix mapping coefficents to data
-    Rij = np.matmul((tresps*np.outer(nt_ones, exptimes)).T, Bij)
-    Dij = idTleft+idTright - np.roll(idTright, -1, axis=0) - np.roll(idTleft, 1, axis=0)
+    Rij = jnp.matmul((tresps*jnp.outer(nt_ones, exptimes)).T, Bij)
+    Dij = idTleft+idTright - jnp.roll(idTright, -1, axis=0) - jnp.roll(idTleft, 1, axis=0)
     regmat = Dij*nd/(drv_con**2*(logt[nt-1]-logt[0]))
-    rvec = np.sum(Rij, axis=1)
+    rvec = jnp.sum(Rij, axis=1)
 
     # NOTE: do not make these JAX arrays. They need to be mutable
     dems = np.zeros([nx, ny, nt])
@@ -136,30 +146,30 @@ def simple_reg_dem_jax(data, errors, exptimes, logt, tresps,
     for i in range(0, nx):
         for j in range(0, ny):
             err = errors[i, j, :]
-            dat0 = np.clip(data[i, j, :], 0.0, None)
-            s = np.log(np.sum((rvec)*((dat0 > 1.0e-2)/err**2))/np.sum((rvec/err)**2)/nt_ones)
+            dat0 = jnp.clip(data[i, j, :], 0.0, None)
+            s = jnp.log(jnp.sum((rvec)*((dat0 > 1.0e-2)/err**2))/jnp.sum((rvec/err)**2)/nt_ones)
             for k in range(0, kmax):
                 # Correct data by f(s)-s*f'(s)
-                dat = (dat0-np.matmul(Rij, ((1-s)*np.exp(s)))) / err
-                mmat = Rij*np.outer(1.0/err, np.exp(s))
-                amat = np.matmul(mmat.T, mmat) + regmat
+                dat = (dat0-jnp.matmul(Rij, ((1-s)*jnp.exp(s)))) / err
+                mmat = Rij*jnp.outer(1.0/err, jnp.exp(s))
+                amat = jnp.matmul(mmat.T, mmat) + regmat
                 try:
                     [c, low] = cho_factor(amat)
                 except:
                     break
-                c2p = np.mean((dat0-np.dot(Rij, np.exp(s)))**2/err**2)
-                deltas = cho_solve((c, low), np.dot(mmat.T, dat))-s
-                deltas *= np.clip(np.max(np.abs(deltas)), None, 0.5/steps[0])/np.max(np.abs(deltas))
+                c2p = jnp.mean((dat0-jnp.dot(Rij, jnp.exp(s)))**2/err**2)
+                deltas = cho_solve((c, low), jnp.dot(mmat.T, dat))-s
+                deltas *= jnp.clip(jnp.max(jnp.abs(deltas)), None, 0.5/steps[0])/jnp.max(jnp.abs(deltas))
                 ds = 1-2*(c2p < chi2_th) # Direction sign; is chi squared too large or too small?
-                c20 = np.mean((dat0-np.dot(Rij, np.exp(s+deltas*ds*steps[0])))**2.0/err**2.0)
-                c21 = np.mean((dat0-np.dot(Rij, np.exp(s+deltas*ds*steps[1])))**2.0/err**2.0)
+                c20 = jnp.mean((dat0-jnp.dot(Rij, jnp.exp(s+deltas*ds*steps[0])))**2.0/err**2.0)
+                c21 = jnp.mean((dat0-jnp.dot(Rij, jnp.exp(s+deltas*ds*steps[1])))**2.0/err**2.0)
                 interp_step = ((steps[0]*(c21-chi2_th)+steps[1]*(chi2_th-c20))/(c21-c20))
-                s += deltas*ds*np.clip(interp_step, steps[0], steps[1])
-                chi2[i, j] = np.mean((dat0-np.dot(Rij, np.exp(s)))**2/err**2)
+                s += deltas*ds*jnp.clip(interp_step, steps[0], steps[1])
+                chi2[i, j] = jnp.mean((dat0-jnp.dot(Rij, jnp.exp(s)))**2/err**2)
                 condition_1 = (ds*(c2p-c20)/steps[0] < tol)*(k > kcon)
-                condition_2 = np.abs(chi2[i, j]-chi2_th) < tol
+                condition_2 = jnp.abs(chi2[i, j]-chi2_th) < tol
                 if condition_1 or condition_2:
                     break
-            dems[i, j, :] = np.exp(s)
+            dems[i, j, :] = jnp.exp(s)
 
     return dems, chi2
