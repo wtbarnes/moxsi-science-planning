b76d9bace277123b5be3fd174e1b535606464665
Will Barnes <will.t.barnes@gmail.com>
2022-10-28 17:17:32 -0400

    pull in custom HK12 algorithm from sunkit-dem

--- mocksipipeline/physics/dem/dem_models.py
+++ mocksipipeline/physics/dem/dem_models.py
@@ -7,7 +7,7 @@ from scipy.interpolate import splrep, splev
 import numpy as np
 from sunkit_dem import GenericModel
 
-from dem_algorithms import simple_reg_dem, sparse_em_init, sparse_em_solve
+from dem_algorithms import simple_reg_dem, sparse_em_init, sparse_em_solve, dn2dem_pos
 try:
     from dem_algorithms_fast import simple_reg_dem_gpu, simple_reg_dem_numba, simple_reg_dem_jax
 except ImportError:
@@ -154,3 +154,38 @@ class CheungModel(GenericModel):
 
     def defines_model_for(self, *args, **kwargs):
         return kwargs.get('model') == 'cheung'
+
+
+class HK12Model(GenericModel):
+
+    def _model(self, alpha=1.0, increase_alpha=1.5, max_iterations=10, guess=None, use_em_loci=False, use_dask=False):
+        errors = np.array([self.data[k].uncertainty.array.squeeze() for k in self._keys]).T
+        dem, edem, elogt, chisq, dn_reg = dn2dem_pos(
+            self.data_matrix.value.T,
+            errors,
+            self.kernel_matrix.value.T,
+            np.log10(self.temperature_bin_centers.to(u.K).value),
+            self.temperature_bin_edges.to(u.K).value,
+            max_iter=max_iterations,
+            reg_tweak=alpha,
+            rgt_fact=increase_alpha,
+            dem_norm0=guess,
+            gloci=use_em_loci,
+            use_dask=use_dask,
+        )
+        dem_unit = self.data_matrix.unit / self.kernel_matrix.unit / self.temperature_bin_edges.unit
+        uncertainty = edem.T * dem_unit
+        em = (dem * np.diff(self.temperature_bin_edges)).T * dem_unit
+        dem = dem.T * dem_unit
+        T_error_upper = self.temperature_bin_centers * (10**elogt -1 )
+        T_error_lower = self.temperature_bin_centers * (1 - 1 / 10**elogt)
+        return {'dem': dem,
+                'uncertainty': uncertainty,
+                'em': em,
+                'temperature_errors_upper': T_error_upper,
+                'temperature_errors_lower': T_error_lower,
+                'chi_squared': np.atleast_1d(chisq)}
+
+    @classmethod
+    def defines_model_for(self, *args, **kwargs):
+        return kwargs.get('model') == 'hk12'
