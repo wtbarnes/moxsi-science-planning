d06f65efc22a6ebb3a82f393a70f8c790420fa40
Ubuntu <ubuntu@ip-172-31-88-174.ec2.internal>
2022-12-07 15:37:05 +0000

    fix jlab setup script

--- jlab-setup.sh
+++ jlab-setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+# Start a JupyterLab server in a screen session
 start_jlab_server () {
-    screen -dm -S "jlab" bash -c "conda activate moxsipipeline; jupyter lab --port=8888 --no-browser --ip=*"
+    screen -dm -S "jlab" bash -c "source $HOME/mambaforge/etc/profile.d/conda.sh; conda activate mocksipipeline; jupyter lab --port=8888 --no-browser --ip=*"
 }
 
-
 # Send a "Ctrl-C" to a detached screen session
-
 stop_jlab_server () {
 	screen -S "jlab" -X stuff "^C"
 }
 
 # Kill a detached screen session
-
 kill_jlab_server () {
 	screen -X -S "jlab" quit
 }
