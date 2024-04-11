1d8dc5decb270ff379c0ce6aca96b49b083be5f6
Will Barnes <will.t.barnes@gmail.com>
2024-04-11 01:19:32 -0400

    move reports to docs structure

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MOXSI Science Reports'
+copyright = '2024, Will Barnes'
+author = 'Will Barnes'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+MOXSI Science Reports
+=======================
+
+This page contains rendered versions of various reports prepared while building the 
+Multi-order X-ray Spectral Imager (MOXSI). These are all meant to be very preliminary
+and only provide a rough draft of more official reports, i.e. presentations.
+
+.. toctree::
+   :maxdepth: 1
+   :glob:
+
+   reports/*
